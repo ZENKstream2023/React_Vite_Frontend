70068d2914ec52dfeaab43ba9c51e40477b38314
Aritz <zenkstreamhelp@gmail.com>
2023-09-18 19:27:50 +0200

    modified:   index.html 	modified:   package-lock.json 	modified:   package.json 	modified:   src/main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
 import { NavLink } from "react-router-dom";
 import "./index.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
 import reactlogo from "../src/assets/images/logo.svg";
 const SearchApp = () => {
 	return (
