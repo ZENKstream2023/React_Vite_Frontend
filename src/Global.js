bbbd01019a3695db2bb67936ca052bf1ac6faa13
Aritz <zenkstreamhelp@gmail.com>
2023-09-18 19:41:32 +0200

    modified:   src/Global.js

--- src/Global.js
+++ src/Global.js
@@ -1,5 +1,6 @@
 var Global = {
-	url: "https://backend-two-black.vercel.app/api" || "http://127.0.0.1:3900/api/",
+	url: "https://backend-two-black.vercel.app/api",
+	// || "http://127.0.0.1:3900/api/",
 };
 
 export default Global;
